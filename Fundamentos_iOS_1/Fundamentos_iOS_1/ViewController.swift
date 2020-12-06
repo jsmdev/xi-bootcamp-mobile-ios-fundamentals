4bf470c9ef4fe7a7323f013a14672ea79ec2144d
José Sancho <jsancho.dev@gmail.com>
2020-12-06 19:46:54 +0100

    Create xcode project

---
+++ Fundamentos_iOS_1/Fundamentos_iOS_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fundamentos_iOS_1
+//
+//  Created by José Sancho on 6/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
